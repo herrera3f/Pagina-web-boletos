cf572e669ca2dacfd3ca7e8e63a96c47570be66a
bimce <benjamin.martinez29@inacapmail.cl>
2023-11-27 17:14:02 -0300

    css_pro

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -164,5 +164,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'brandonstibenmarinherrera@gmail.com'
-EMAIL_HOST_PASSWORD = 'asft dcwt lvys fxaw'
+EMAIL_HOST_USER = 'aeroreservas0@gmail.com'
+EMAIL_HOST_PASSWORD = 'btid oske wodh fjtn'
